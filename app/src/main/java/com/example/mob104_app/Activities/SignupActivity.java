d2b40c11a1792d59431d9b60395e1fdf4a996bf7
cuongmcph27903 <94911808+cuongmc2407@users.noreply.github.com>
2023-05-15 23:34:30 +0700

    layout signup, forget password

---
+++ app/src/main/java/com/example/mob104_app/Activities/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.example.mob104_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mob104_app.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
