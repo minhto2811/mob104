e6f68314cefa67b25715d1db48882247b751ec36
HP <quangnv572003@gmail.com>
2023-05-18 14:26:18 +0700

    Giao diện  chi tiết sản phẩm

--- app/src/main/java/com/example/mob104_app/Activities/ProductDetailActivity.java
+++ app/src/main/java/com/example/mob104_app/Activities/ProductDetailActivity.java
@@ -12,6 +12,5 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
-
     }
 }
