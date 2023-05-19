56f39ab00dad0858d7b4e959af3df01d35dcf7a9
HP <quangnv572003@gmail.com>
2023-05-19 19:10:30 +0700

    Gd Cud san pham

---
+++ app/src/main/java/com/example/mob104_app/Activities/Products/ViewProductActivity.java
@@ -0,0 +1,16 @@
+package com.example.mob104_app.Activities.Products;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mob104_app.R;
+
+public class ViewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fix_product);
+    }
+}
